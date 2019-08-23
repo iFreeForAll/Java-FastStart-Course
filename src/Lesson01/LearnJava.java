9cf2f4cd52feaf72065745bea854394b1aba367c
Oleg <guitar_hero@me.com>
2019-08-23 17:21:01 +0200

    Added comments to the Lesson01. New class "LearnJava" for HW.

---
+++ src/Lesson01/LearnJava.java
@@ -0,0 +1,8 @@
+package Lesson01;
+
+//first homework
+public class LearnJava {
+    public static void main(String[] args) {
+        System.out.println("I will learn Java!"); //display in console the phrase
+    }
+}
--- src/Lesson01/Lesson01.java
+++ src/Lesson01/Lesson01.java
@@ -1,7 +1,8 @@
 package Lesson01;
 
+//code from the first lesson of the course
 public class Lesson01 {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Hello, world!"); //simple output to the console
     }
 }
